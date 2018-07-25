1c5012fecb08176c46ade6fc86e2021c1aa56d95
Kirill Maslov <polaris_@mail.ru>
2018-07-26 00:03:54 +0300

    TH_ELT

--- sha3/sph_keccak.h
+++ sha3/sph_keccak.h
@@ -71,6 +71,8 @@ static sph_u64 internal_xor64(sph_u64 a,sph_u64 b);
 
 static sph_u64 internal_mov64(sph_u64 a);
 
+static void internal_th_elt(sph_u64 t,sph_u64 c0,sph_u64 c1,sph_u64 c2,sph_u64 c3,sph_u64 c4,sph_u64 d0,sph_u64 d1,sph_u64 d2,sph_u64 d3,sph_u64 d4);
+
 /**
  * This structure is a context for Keccak computations: it contains the
  * intermediate values and some data from the last entered block. Once a
